69dc0275789fdeeb90da0994f725e2c6d511d26e
Dacheee <dace.kalisuka@gmail.com>
2021-07-16 20:03:02 +0300

    Initial commit

---
+++ src/arraysLesson8.java
@@ -0,0 +1,27 @@
+public class arraysLesson8 {
+    public static void main(String[] args) {
+
+        int [] newArray = new int[3];
+        newArray [0] = 5;
+        newArray [1] = 10;
+        newArray [2] = 15;
+
+        System.out.println("This is the first element : " + newArray[0]);
+
+        int [] temperatures = {1,2,3,4,5,6,7};
+
+        System.out.println("number 5 = " + temperatures[4]);
+        System.out.println("In temperature array we have " + temperatures.length);
+        System.out.println("The last element of teh array is :" + temperatures[temperatures.length -1]);
+
+        int [] weatherForecast = new int [15];
+        weatherForecast [0] = 20;
+        weatherForecast [1] = 21;
+        
+
+
+
+
+
+    }
+}
